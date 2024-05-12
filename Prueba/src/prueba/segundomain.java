f28875294276c9dd74279a3e41db4bc3e1a52b95
adri_ <adri_@192.168.61.61>
2024-05-12 12:37:10 +0200

    segundo commit

--- Prueba/src/prueba/segundomain.java
+++ Prueba/src/prueba/segundomain.java
@@ -4,7 +4,8 @@ public class segundomain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("nueva clase para probar");
+		System.out.println("Mensje para segundo main");
 	}
 
 }
